d8e01a669b203c40a6bdc95605c6c5a099bb284c
Andrés Mechali <andresm@composable.finance>
2022-08-18 13:43:10 +0200

    refactor and fix tests

--- subsquid/src/types/v2401.ts
+++ subsquid/src/types/v2401.ts
@@ -17,10 +17,28 @@ export interface Fee {
   assetId: CurrencyId
 }
 
+export type VestingScheduleIdSet = VestingScheduleIdSet_All | VestingScheduleIdSet_One | VestingScheduleIdSet_Many
+
+export interface VestingScheduleIdSet_All {
+  __kind: 'All'
+}
+
+export interface VestingScheduleIdSet_One {
+  __kind: 'One'
+  value: bigint
+}
+
+export interface VestingScheduleIdSet_Many {
+  __kind: 'Many'
+  value: bigint[]
+}
+
 export interface VestingSchedule {
+  vestingScheduleId: bigint
   window: VestingWindow
   periodCount: number
   perPeriod: bigint
+  alreadyClaimed: bigint
 }
 
 export type VestingWindow = VestingWindow_MomentBased | VestingWindow_BlockNumberBased
